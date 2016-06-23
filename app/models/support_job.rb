da5646c12d29e69f29c7fae79e16a55379d597d4
Camron Khan <camron.khan@gmail.com>
2016-06-23 13:43:28 -0500

    updated relations and constraints

--- app/models/support_job.rb
+++ app/models/support_job.rb
@@ -1,2 +1,10 @@
 class SupportJob < ActiveRecord::Base
+	
+	# Relations
+	belongs_to :product
+	belongs_to :support_type
+	belongs_to :agent
+
+	# Polymorphic Attributes
+	has_many :notes, as: :annotatable
 end
