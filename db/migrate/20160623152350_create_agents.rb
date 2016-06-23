48517702d3ed57a5417d7f4880fcc575abb51cb0
Camron Khan <camron.khan@gmail.com>
2016-06-23 11:20:41 -0500

    added 24x7 attribute

--- db/migrate/20160623152350_create_agents.rb
+++ db/migrate/20160623152350_create_agents.rb
@@ -2,6 +2,7 @@ class CreateAgents < ActiveRecord::Migration
   def change
     create_table :agents do |t|
       t.string :name
+      t.boolean :operates24x7
       t.references :split, index: true, foreign_key: true
       t.references :support_function, index: true, foreign_key: true
       t.references :support_center, index: true, foreign_key: true
