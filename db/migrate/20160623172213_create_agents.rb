d15d84e8b8120a6769fae3be2f1ee4145476b574
Camron Khan <camron.khan@gmail.com>
2016-06-23 12:31:52 -0500

    added not null constraints

--- db/migrate/20160623172213_create_agents.rb
+++ db/migrate/20160623172213_create_agents.rb
@@ -3,8 +3,8 @@ class CreateAgents < ActiveRecord::Migration
     create_table :agents do |t|
       t.string :name, null: false
       t.boolean :operates24x7, null: false, default: false
-      t.references :support_function, index: true, foreign_key: true
-      t.references :support_center, index: true, foreign_key: true
+      t.references :support_function, index: true, foreign_key: true, null: false
+      t.references :support_center, index: true, foreign_key: true, null: false
       t.timestamps null: false
     end
     add_index :agents, :name, unique: true
