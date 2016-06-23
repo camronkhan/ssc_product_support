15926d641e0dd670cc372f8301aa56fcb3d84a64
Camron Khan <camron.khan@gmail.com>
2016-06-23 08:49:30 -0500

    modeled case

---
+++ app/models/case.rb
@@ -0,0 +1,4 @@
+class Case < ActiveRecord::Base
+	# Relations
+	belongs_to :caseable, polymorphic: true
+end
---
+++ db/migrate/20160623134546_create_cases.rb
@@ -0,0 +1,12 @@
+class CreateCases < ActiveRecord::Migration
+  def change
+    create_table :cases do |t|
+      t.string :queue
+      t.string :description
+
+      t.timestamps null: false
+    end
+    add_index :cases, :queue, unique: true
+    add_index :cases, [:caseable_type, :caseable_id], name: 'caseable_index'
+  end
+end
