f226ba28d10ed62abe2c1876bf58e850eec812c5
Camron Khan <camron.khan@gmail.com>
2016-06-23 14:10:36 -0500

    added not null constraints

--- db/migrate/20160623185700_create_user_roles.rb
+++ db/migrate/20160623185700_create_user_roles.rb
@@ -1,9 +1,8 @@
 class CreateUserRoles < ActiveRecord::Migration
   def change
     create_table :user_roles do |t|
-      t.string :name
-      t.string :description
-
+      t.string :name, null: false
+      t.string :description, null: false
       t.timestamps null: false
     end
     add_index :user_roles, :name, unique: true
