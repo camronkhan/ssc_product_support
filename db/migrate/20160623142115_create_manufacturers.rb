2a147b30b9f783fce2bc028e6f5e47d4ec72335e
Camron Khan <camron.khan@gmail.com>
2016-06-23 09:21:53 -0500

    modeled manufacturer

---
+++ db/migrate/20160623142115_create_manufacturers.rb
@@ -0,0 +1,10 @@
+class CreateManufacturers < ActiveRecord::Migration
+  def change
+    create_table :manufacturers do |t|
+      t.string :name
+      t.string :website_url
+      t.timestamps null: false
+    end
+    add_index :manufacturers, :name, unique: true
+  end
+end
