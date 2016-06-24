15b997cf713fc9ac199e1bb2a083ea5c4958313a
Camron Khan <camron.khan@gmail.com>
2016-06-24 15:33:08 -0500

    added operation time model validations, tests, and fixtures

--- app/models/operation_time.rb
+++ app/models/operation_time.rb
@@ -3,4 +3,13 @@ class OperationTime < ActiveRecord::Base
 	# Relations
 	belongs_to :agent
 	belongs_to :day
+
+	# Validations
+	validates :open, presence: true
+	validates :close, presence: true
+	validates :agent, presence: true
+	validates_associated :agent
+	validates :day, presence: true
+	validates_associated :day
+
 end
--- test/models/operation_time_test.rb
+++ test/models/operation_time_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class OperationTimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	fixtures :agents, :days
+
+	def setup
+		@new_operation_time = OperationTime.create(
+			open: '2016-06-23 12:33:24',
+			close: '2016-06-23 12:33:24',
+			agent: agents(:valid_agent),
+			day: days(:valid_day)
+		)
+	end
+
+	test "valid note must be saved" do
+		assert @new_operation_time.valid?
+		assert @new_operation_time.save
+	end
+
+	test "note open time must be present" do
+		@new_operation_time.open = ''
+		assert @new_operation_time.invalid?
+		assert_not @new_operation_time.save
+	end
+
+	test "note close time must be present" do
+		@new_operation_time.close = ''
+		assert @new_operation_time.invalid?
+		assert_not @new_operation_time.save
+	end
+
+	test "agent must be associated with an agent" do
+		@new_operation_time.agent = nil
+		assert @new_operation_time.invalid?
+		assert_not @new_operation_time.save
+	end
+
+	test "agent must be associated with a day" do
+		@new_operation_time.day = nil
+		assert @new_operation_time.invalid?
+		assert_not @new_operation_time.save
+	end
+
 end
