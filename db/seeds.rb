fa37985a943a6fc3ca80972a0fd5e03a61cfb562
Camron Khan <camron.khan@gmail.com>
2016-06-24 08:37:12 -0500

    added seed data and updated tests/fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+UserRole.create ([
+	{ name: 'Administrator', description: 'Create, read, update, and delete all objects' },
+	{ name: 'Subject Matter Expert', description: 'Create, update, and delete products; Read-only for all other objects' },
+	{ name: 'Internal User', description: 'Read-only for all objects' },
+	{ name: 'External User', description: 'Read-only for non-protected information' }
+])
+
+Day.create([
+	{ day: 'Sunday' },
+	{ day: 'Monday' },
+	{ day: 'Tuesday' },
+	{ day: 'Wednesday' },
+	{ day: 'Thursday' },
+	{ day: 'Friday' },
+	{ day: 'Saturday' }
+])
+
+SupportCenter.create([
+	{ name: 'Support Center Information Not Available' },
+	{ name: 'Other' },
+	{ name: 'North America & Latin America (NALA) Solution Support Center (SSC)' },
+	{ name: 'Europe, Middle East, & Africa (EMEA) Solution Support Center (SSC)' },
+	{ name: 'Asia & Pacific (AP) Solution Support Center (SSC)' },
+	{ name: 'North America (NA) Marketing Call Center (MCC)' }
+])
+
+SupportType.create([
+	{ name: 'Support Type Information Not Available' },
+	{ name: 'Other' },
+	{ name: 'Service Desk' },
+	{ name: 'Technical Support' }
+])
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,32 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  	
-  	fixtures :user_roles
-  	
-	def setup
-		@valid_user = User.new(core_id: "zzz999",
-						 	   first_name: "First",
-							   last_name: "Last",
-							   email: "user@example.com",
-		                 	   password: "foobar",
-		                 	   password_confirmation: "foobar",
-		                 	   user_role_id: 1)
-	end
 
 	test "should save valid user" do
-		assert @valid_user.save
-	end
-
-	test "should not save user without password" do
-		user = User.new
-		assert_not user.save
-	end
-
-	test "should not save user with password less than 6 characters" do
-		user = @valid_user
-		user.password = "foo"
-		assert_not user.save
+		assert users(:valid).save
 	end
 
 end
