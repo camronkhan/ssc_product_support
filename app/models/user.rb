0bcece89402ffe61d07874d6e11b6b51de38b722
Camron Khan <camron.khan@gmail.com>
2016-06-24 10:34:55 -0500

    added user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   	has_secure_password
 
   	# Validations
-  	validates :password, presence: true, length: { minimum: 6 }
-  	validates :password_confirmation, presence: true
+  	validates :core_id, presence: true, uniqueness: { case_sensitive: false }
+  	validates :first_name, presence: true
+  	validates :last_name, presence: true
+  	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /@/i }
+  	validates :password, length: { minimum: 6 }
+  	validates_associated :user_role
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,25 +2,78 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-	test "should save valid user" do
-		user = users(:valid)
-		user.password = 'password'
-		user.password_confirmation = 'password'
-		assert user.save
+	def setup
+		@user = users(:valid)
+		@user.password = 'password'
+		@user.password_confirmation = 'password'
+
+		@duplicate = User.new(core_id: 'nmq687',
+						     first_name: 'Camron',
+					         last_name: 'Khan',
+						     email: 'camron.khan@motorolasolutions.com',
+						     password_digest: 'hashpass',
+						     user_role_id: 999)
+		@duplicate.password = 'password'
+		@duplicate.password_confirmation = 'password'
 	end
 
-	test "should not save user without password" do
-		user = users(:valid)
-		user.password = ''
-		user.password_confirmation = 'password'
-		assert_not user.save
+	test "valid user must be saved" do
+		assert @user.save
 	end
 
-	test "should not save user without password confirmation" do
-		user = users(:valid)
-		user.password = 'password'
-		user.password_confirmation = ''
-		assert_not user.save
+	test "duplicate user must not be saved" do
+	    assert_not @duplicate.save
+	end
+
+	test "core id must be present" do
+		@user.core_id = ''
+		assert_not @user.save
+	end
+
+	test "first name must be present" do
+		@user.first_name = ''
+		assert_not @user.save
+	end
+
+	test "last name must be present" do
+		@user.last_name = ''
+		assert_not @user.save
+	end
+
+	test "email address must be present" do
+		@user.email = ''
+		assert_not @user.save
+	end
+
+	test "email address must contain @ symbol" do
+		@user.email = 'camron.khan#motorolasolutions.com'
+		assert_not @user.save
+	end
+
+	test "password must be present" do
+		@user.password = nil
+		assert_not @user.save
+	end
+
+	test "password confirmation must be present" do
+		@user.password_confirmation = ''
+		assert_not @user.save
+	end
+
+	test "password and confirmation must match" do
+		@user.password_confirmation = 'password1'
+		assert_not @user.save
+	end
+
+	test "password length must be at least 6 characters" do
+		@user.password = 'pass'
+		@user.password_confirmation = 'pass'
+		assert_not @user.save
+	end
+
+	test "user must be associated with a user role" do
+		@user.user_role = nil
+		assert_not @user.invalid?
 	end
 
 end
