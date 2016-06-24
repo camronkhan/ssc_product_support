09bb77a5fa9961e5736dcbe691b7c3862f40137d
Camron Khan <camron.khan@gmail.com>
2016-06-24 16:47:45 -0500

    added support center model validations, tests, and fixtures

--- test/models/support_center_test.rb
+++ test/models/support_center_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SupportCenterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@new_center = SupportCenter.create(
+			name: 'NewString'
+		)
+
+		@duplicate_center = SupportCenter.create(
+			name: 'MyString'
+		)
+	end
+
+	test "valid support center must be saved" do
+		assert @new_center.valid?
+		assert @new_center.save
+	end
+
+	test "duplicate support center must not be saved" do
+		assert @duplicate_center.invalid?
+	    assert_not @duplicate_center.save
+	end
+
+	test "support center name must be present" do
+		@new_center.name = ''
+		assert @new_center.invalid?
+		assert_not @new_center.save
+	end 
+
 end
