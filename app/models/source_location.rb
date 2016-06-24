43afc55a215f4ce266a2efb6805ea388b213e4cd
Camron Khan <camron.khan@gmail.com>
2016-06-24 16:36:10 -0500

    added source location model validations, tests, and fixtures

--- app/models/source_location.rb
+++ app/models/source_location.rb
@@ -2,4 +2,8 @@ class SourceLocation < ActiveRecord::Base
 
 	# Relations
 	has_many :products
+
+	# Validations
+	validates :name, presence: true, uniqueness: { case_sensitive: false }
+	
 end
--- test/models/source_location_test.rb
+++ test/models/source_location_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SourceLocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@new_location = SourceLocation.create(
+			name: 'NewString'
+		)
+
+		@duplicate_location = SourceLocation.create(
+			name: 'MyString'
+		)
+	end
+
+	test "valid location must be saved" do
+		assert @new_location.valid?
+		assert @new_location.save
+	end
+
+	test "duplicate location must not be saved" do
+		assert @duplicate_location.invalid?
+	    assert_not @duplicate_location.save
+	end
+
+	test "location name must be present" do
+		@new_location.name = ''
+		assert @new_location.invalid?
+		assert_not @new_location.save
+	end 
+
 end
