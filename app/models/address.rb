dfe96fad58261c4b74dbc0ce299dc22109adfd94
Camron Khan <camron.khan@gmail.com>
2016-06-24 12:32:56 -0500

    added address model validations, tests, and fixtures

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,13 @@ class Address < ActiveRecord::Base
 	
 	# Relations
 	belongs_to :addressable, polymorphic: true
+
+	# Validations
+	validates :company, presence: true
+	validates :line_1, presence: true
+	validates :city, presence: true
+	validates :state_province, presence: true
+	validates :postal_code, presence: true
+	validates :country, presence: true
+	
 end
