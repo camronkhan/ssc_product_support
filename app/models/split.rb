a5386fc8f9889901686b21b012ffbe3681e7ec94
Camron Khan <camron.khan@gmail.com>
2016-06-24 16:42:46 -0500

    added split model validations, tests, and fixtures

--- app/models/split.rb
+++ app/models/split.rb
@@ -2,4 +2,10 @@ class Split < ActiveRecord::Base
 	
 	# Relations
 	belongs_to :agent
+
+	# Validations
+	validates :queue, presence: true, uniqueness: { case_sensitive: false }
+	validates :agent, presence: true
+	validates_associated :agent
+	
 end
--- test/models/split_test.rb
+++ test/models/split_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class SplitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	fixtures :agents
+
+	def setup
+		@new_queue = Split.create(
+			queue: 'NewString',
+			description: 'NewString',
+			agent: agents(:valid_agent)
+		)
+
+		@duplicate_queue = Split.create(
+			queue: 'MyString',
+			description: 'MyString',
+			agent: agents(:valid_agent)
+		)
+	end
+
+	test "valid agent must be saved" do
+		assert @new_queue.valid?
+		assert @new_queue.save
+	end
+
+	test "duplicate agent must not be saved" do
+		assert @duplicate_queue.invalid?
+	    assert_not @duplicate_queue.save
+	end
+
+	test "queue must be present" do
+		@new_queue.queue = ''
+		assert @new_queue.invalid?
+		assert_not @new_queue.save
+	end
+
+	test "queue must be associated with an agent" do
+		@new_queue.agent = nil
+		assert @new_queue.invalid?
+		assert_not @new_queue.save
+	end
+
 end
