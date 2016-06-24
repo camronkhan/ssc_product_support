13d50274b86ceed3e8a067fae7bb38b9a580d866
Camron Khan <camron.khan@gmail.com>
2016-06-24 15:20:57 -0500

    added note model validations, tests, and fixtures

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,8 @@ class Note < ActiveRecord::Base
 	
 	# Relations
 	belongs_to :annotatable, polymorphic: true
+
+	# Validations
+	validates :info, presence: true
+	
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@new_note = Note.create(
+			info: 'NewString'
+		)
+	end
+
+	test "valid note must be saved" do
+		assert @new_note.valid?
+		assert @new_note.save
+	end
+
+	test "note info must be present" do
+		@new_note.info = ''
+		assert @new_note.invalid?
+		assert_not @new_note.save
+	end
 end
