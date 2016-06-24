46853fafa79b6949ab8a2f9df6b6ed6b908618b9
Camron Khan <camron.khan@gmail.com>
2016-06-24 13:04:42 -0500

    added case queue model validations, tests, and fixtures

--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class CaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	def setup
+		@new_queue = Case.create(
+			queue: 'NewString',
+			description: 'NewString'
+		)
+
+		@duplicate_queue = Case.create(
+			queue: 'MyString',
+			description: 'MyString'
+		)
+	end
+
+	test "valid agent must be saved" do
+		assert @new_queue.valid?
+		assert @new_queue.save
+	end
+
+	test "duplicate agent must not be saved" do
+		assert @duplicate_queue.invalid?
+	    assert_not @duplicate_queue.save
+	end
+
+	test "queue must be present" do
+		@new_queue.queue = ''
+		assert @new_queue.invalid?
+		assert_not @new_queue.save
+	end
 end
