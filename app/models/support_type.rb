a8a1337b99b103bdf61e9179e289d4020fdd3372
Camron Khan <camron.khan@gmail.com>
2016-06-24 16:52:45 -0500

    added support type model validations, tests, and fixtures

--- app/models/support_type.rb
+++ app/models/support_type.rb
@@ -2,4 +2,8 @@ class SupportType < ActiveRecord::Base
 
 	# Relations
 	has_many :support_jobs
+
+	# Validations
+	validates :name, presence: true, uniqueness: { case_sensitive: false }
+	
 end
--- test/models/support_type_test.rb
+++ test/models/support_type_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SupportTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@new_type = SupportType.create(
+			name: 'NewString'
+		)
+
+		@duplicate_type = SupportType.create(
+			name: 'MyString'
+		)
+	end
+
+	test "valid type must be saved" do
+		assert @new_type.valid?
+		assert @new_type.save
+	end
+
+	test "duplicate type must not be saved" do
+		assert @duplicate_type.invalid?
+	    assert_not @duplicate_type.save
+	end
+
+	test "type name must be present" do
+		@new_type.name = ''
+		assert @new_type.invalid?
+		assert_not @new_type.save
+	end
+
 end
