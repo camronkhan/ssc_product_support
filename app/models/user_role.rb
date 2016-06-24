60a0e7d27adc3f17ca9fdf7c55e56231a690b2f5
Camron Khan <camron.khan@gmail.com>
2016-06-24 16:56:34 -0500

    added user role model validations, tests, and fixtures

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,4 +2,9 @@ class UserRole < ActiveRecord::Base
 
 	# Relations
 	has_many :users
+
+	# Validations
+	validates :name, presence: true, uniqueness: { case_sensitive: false }
+	validates :description, presence: true
+	
 end
--- test/models/user_role_test.rb
+++ test/models/user_role_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserRoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@new_role = UserRole.create(
+			name: 'NewString',
+			description: 'NewString'
+		)
+
+		@duplicate_role = UserRole.create(
+			name: 'MyString',
+			description: 'MyString'
+		)
+	end
+
+	test "valid role must be saved" do
+		assert @new_role.valid?
+		assert @new_role.save
+	end
+
+	test "duplicate role must not be saved" do
+		assert @duplicate_role.invalid?
+	    assert_not @duplicate_role.save
+	end
+
+	test "role name must be present" do
+		@new_role.name = ''
+		assert @new_role.invalid?
+		assert_not @new_role.save
+	end
+
+	test "role description must be present" do
+		@new_role.description = ''
+		assert @new_role.invalid?
+		assert_not @new_role.save
+	end
+
 end
