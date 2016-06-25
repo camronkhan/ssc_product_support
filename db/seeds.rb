f20e4df735b5f653a696ba3723b4dafdd7f8b1a0
Camron Khan <camron.khan@gmail.com>
2016-06-25 12:36:18 -0500

    updated servicer and address seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,49 +65,45 @@ SourceType.create([
 	{ name: 'External' }
 ])
 
-Servicer.create([
-	{ name: 'Not Supported' },
+Company.create([
+	{ name: 'Not Applicable' },
 	{ name: 'Unknown' },
-	{ name: 'Motorola Solutions Service Center - El Paso' },
-	{ name: 'Motorola Solutions Service Center - Elgin' },
-	{ name: 'Motorola Federal Technical Center', internal_phone: 'XGPS-CCS Fed Govt', external_phone: '800-969-6680', address: '10105 Senate Dr, Lanham, MD 20706' },
-	{ name: 'Motorola Canada Radio Repair Depot', internal_phone: 'XGPS-CCS Canada', external_phone: '800-543-3222', address: '8133 Warden Ave, Markham, Ontario, Canada L6G183' },
-	{ name: 'Infrastructure Depot Operations (IDO)', internal_phone: 'XGPS-SSC Return Authorization', address: '2214 Galvin Dr, Elgin, IL 60124' },
-	{ name: 'CCS Business Radio', internal_phone: 'XGPS-CCS RBR Support' },
-	{ name: 'Vertex Standard Repair Center', external_phone: '800-283-7839', address: '7201-A Intermodal Drive, Louisville KY 40258' },
-	{ name: 'Zebra Technologies', external_phone: '800-653-5350' },
-	{ name: 'Futurecom', internal_phone: 'See Notes', external_phone: '800-701-9180' },
-	{ name: 'AdvanceTec Industries', external_phone: '866-881-8211' },
-	{ name: 'Astron Corp', external_phone: '949-458-7277' },
-	{ name: 'AvtechTyee', external_phone: '425-290-3100 ' },
-	{ name: 'Cadex Electronics', external_phone: '800-565-5228' },
-	{ name: 'David Clark Co', external_phone: '508-751-5800' },
-	{ name: 'Gamber Johnson', external_phone: '800-456-6868 x301' },
-	{ name: 'Giant International', external_phone: '800-353-2729' },
-	{ name: 'GAI-Tronics', external_phone: '800-492-1212' },
-	{ name: 'Global Data Specialists', external_phone: '800-451-3464' },
-	{ name: 'International Radio Repair', internal_phone: 'XGPS-CCS INTL' },
-	{ name: 'Microwave Data Systems', external_phone: '585-242-9600' },
-	{ name: 'Microwave Network', external_phone: '800-545-0334' },
-	{ name: 'Newmar', external_phone: '800-854-3906' },
-	{ name: 'Business Radio Repair', internal_phone: 'XGPS-CCS RBR Dealer Support' },
-	{ name: 'Telex', external_phone: '800-898-6723' },
-	{ name: 'TPL Communications', external_phone: '323-256-3000' },
-	{ name: 'Zetron', external_phone: '425-820-6363' },
-	{ name: 'Lorain Reltec Network', external_phone: '800-752-8733 x116' },
-	{ name: 'Cambium Networks', external_phone: '888-863-5250' },
-	{ name: 'CCS Consumer Radio', internal_phone: 'XGPS-CCS Consumer' }
+	{ name: 'Motorola Solutions', website: 'http://www.motorolasolutions.com/' },
+	{ name: 'Vertex Standard', website: 'http://www.vertexstandard.com/' },
+	{ name: 'Emergency CallWorks', website: 'https://www.emergencycallworks.com/' },
+	{ name: 'Zebra Technologies', website: 'https://www.zebra.com/' },
+	{ name: 'Motorola Mobility', website: 'http://www.motorola.com/' },
+	{ name: 'Cambium Networks', website: 'http://www.cambiumnetworks.com/' },
+	{ name: 'NICE Systems', website: 'http://www.nice.com/' },
+	{ name: 'Airbus DS Communications', website: 'http://www.airbus-dscomm.com/' },
+	{ name: 'VMware', website: 'http://www.vmware.com/' },
+	{ name: 'Futurecom', website: 'http://www.futurecom.com/' }
 ])
 
 Servicer.create([
-	{ name: 'Motorola Solutions Service Center, El Paso', line_1: '1220 Don Haskins Drive', line_2: 'Suite A', city: 'El Paso', state_province: 'TX', postal_code: '79936', country: 'United States' },
-	{ name: 'Motorola Solutions Service Center, Elgin - Subscriber', line_1: '2204 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Motorola Solutions Service Center, Elgin - Laptops', line_1: '2208 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Motorola Solutions Service Center, Elgin - Test Equipment', line_1: '2216 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Motorola Solutions Infrastructure Depot Operations (IDO)', line_1: '2214 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Motorola Solutions Federal Technical Center (FTC)', line_1: '10105 Senate Drive', city: 'Lanham', state_province: 'MD', postal_code: '20706', country: 'United States' },
-	{ name: 'Motorola Solutions Canadian Technical Logistics Center (CTLC)', line_1: '181 Whitehall Drive', city: 'Markham', state_province: 'Ontario', postal_code: 'L3R 9T1', country: 'Canada' },
-	{ name: 'Motorola Solutions Returned Goods', line_1: '2222 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Motorola Solutions Warranty Department', line_1: '2222 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
-	{ name: 'Vertex Standard Service Center', line_1: '7201-A Intermodal Drive', city: 'Louisville', state_province: 'KY', postal_code: '40258', country: 'United States' }
+	{ name: 'Motorola Solutions Service Center, El Paso' },
+	{ name: 'Motorola Solutions Service Center, Elgin - Subscriber' },
+	{ name: 'Motorola Solutions Service Center, Elgin - Laptops' },
+	{ name: 'Motorola Solutions Service Center, Elgin - Test Equipment' },
+	{ name: 'Motorola Solutions Infrastructure Depot Operations (IDO)' },
+	{ name: 'Motorola Solutions Federal Technical Center (FTC)' },
+	{ name: 'Motorola Solutions Canadian Technical Logistics Center (CTLC)' },
+	{ name: 'Motorola Solutions Returned Goods' },
+	{ name: 'Motorola Solutions Warranty Department' },
+	{ name: 'Vertex Standard Service Center' },
+	{ name: 'Futurecom' }
+])
+
+Address.create([
+	{ addressable_type: 'Servicer', addressable_id: 1, name: 'Motorola Solutions Service Center', line_1: '1220 Don Haskins Drive', line_2: 'Suite A', city: 'El Paso', state_province: 'TX', postal_code: '79936', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 2, name: 'Motorola Solutions Service Center', line_1: '2204 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 3, name: 'Motorola Solutions Service Center', line_1: '2208 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 4, name: 'Motorola Solutions Service Center', line_1: '2216 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 5, name: 'Motorola Solutions IDO Repair', line_1: '2214 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 6, name: 'Motorola Solutions Federal Technical Center', line_1: '10105 Senate Drive', city: 'Lanham', state_province: 'MD', postal_code: '20706', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 7, name: 'Motorola Solutions Canadian Technical Logistics Center', line_1: '181 Whitehall Drive', city: 'Markham', state_province: 'Ontario', postal_code: 'L3R 9T1', country: 'Canada' },
+	{ addressable_type: 'Servicer', addressable_id: 8, name: 'Motorola Solutions Returned Goods', line_1: '2222 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 9, name: 'Motorola Solutions Warranty Department', line_1: '2222 Galvin Drive', city: 'Elgin', state_province: 'IL', postal_code: '60124', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 10, name: 'Vertex Standard Service Center', line_1: '7201-A Intermodal Drive', city: 'Louisville', state_province: 'KY', postal_code: '40258', country: 'United States' },
+	{ addressable_type: 'Servicer', addressable_id: 11, name: 'Futurecom Systems Group Inc', line_1: '3277 Langstaff Road', city: 'Concord', state_province: 'Ontario', postal_code: 'L4K 5P8', country: 'Canada' }
 ])
