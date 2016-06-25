da66eb0645d9af1377b0f5ec7036c18c55fbb486
Camron Khan <camron.khan@gmail.com>
2016-06-25 12:40:31 -0500

    removed annotatable association

--- app/models/support_center.rb
+++ app/models/support_center.rb
@@ -3,9 +3,6 @@ class SupportCenter < ActiveRecord::Base
 	# Relations
 	has_many :agents
 
-	# Polymorphic Attributes
-	has_many :notes, as: :annotatable
-
 	# Validations
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	
--- app/models/support_function.rb
+++ app/models/support_function.rb
@@ -3,9 +3,6 @@ class SupportFunction < ActiveRecord::Base
 	# Relations
 	has_many :agents
 
-	# Polymorphic Attributes
-	has_many :notes, as: :annotatable
-
 	# Validations
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	
