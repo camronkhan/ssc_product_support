03d9ab502747629fc463f4735583c8d6375d0d19
Camron Khan <camron.khan@gmail.com>
2016-06-25 12:37:17 -0500

    removed addressable and phonable associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,8 +4,6 @@ class Company < ActiveRecord::Base
 	has_many :products
 
 	# Polymorphic Attributes
-	has_many :addresses, as: :addressable
-	has_many :phones, as: :phonable
 	has_many :notes, as: :annotatable
 
 	# Validations
